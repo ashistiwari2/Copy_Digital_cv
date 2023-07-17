77bb15a48fbc7645f750a9418a2b9458779c4731
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2023-07-17 21:43:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -167,8 +167,6 @@ with st.sidebar:
 		components.html(ey_bade_1['certi'],height=335)
 		components.html(scan['scan'],height=250)
 		components.html(ey_bade_2['certi'],height=335)
-		
-# 		components.html('''
 # 			<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
 #             <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ashistiwari2" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/ashistiwari2?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: auto; height:540px;"> <div id="retainable-rss-embed" 
 #     data-rss="https://medium.com/feed/retainable,https://medium.com/feed/data-science-in-your-pocket"
